38a0b8a0f860fd07132a7179095acf91182164a3
isaac <isaac@isaac-Aspire-E1-572>
2016-08-13 23:48:14 -0300

    Atualizando a estrutura basica do projeto - 13/08/2016

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once './indexControl.php';
         <meta name="robots" content="noindex, nofollow" />
 
         <link rel="shortcut icon" href=""/>
-        
+
         <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.css"/>
 
         <script type="text/javascript" src="asset/_cdn/lib/jquery.js"></script>
@@ -22,7 +22,28 @@ require_once './indexControl.php';
 
     <body>
         <div id="LoginBox" class="">
-            
+            <form id="" class="" method="POST" action="" enctype="multipart/form-data">
+                <div id="" class="">
+                    <label for="user_login">Usuários: </label>
+                    <input type="text" name="user_login" id="user_login" placeholder="E-mail/Usuários"/>
+                </div>
+                <div id="" class="">
+                    <label for="user_login">Senha: </label>
+                    <input type="password" name="user_password" id="user_password" placeholder="E-mail/Usuários"/>
+                </div>
+                <div id="" class="">
+                    <button id="" class="">Fazer Login</button>
+                </div>
+            </form>
+        </div>
+
+        <div id="LoginBox" class="">
+            <form id="" class="" method="POST" action="" enctype="multipart/form-data">
+                <div id="" class="">
+                    <label for="user_login">Usuários: </label>
+                    <input type="text" name="user_login" id="user_login" placeholder="E-mail/Usuários"/>
+                </div>
+            </form>
         </div>
     </body>
 </html>
