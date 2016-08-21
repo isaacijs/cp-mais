c49f4c518c89b58c8b19c72099aa5af098eb38ca
isaac <isaac@isaac-Aspire-E1-572>
2016-08-20 21:39:25 -0300

    Revert "Criação das Caixa de LogIn e SignUp - 14/08/2016"
    This reverts commit 631a893d85fa27309058ac6748df8e1e7d41f94a.

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ require_once './_app/Config.inc.php';
 $setExe = filter_input(INPUT_GET, 'exe', FILTER_DEFAULT);
 $dataPost = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-$msgSystem = filter_input(INPUT_GET, 'msy', FILTER_DEFAULT);
-
 if ($setExe === 'logoff' || $setExe === 'restrito'):
     unset($_SESSION['userlogin']);
 endif;
@@ -20,7 +18,7 @@ endif;
 if ($dataPost and isset($dataPost['SendLogin'])):
     $fazerlogin = new Login;
     $fazerlogin->ExeLogin($dataPost);
-    if ($fazerlogin->getResult()):
+    if($fazerlogin->getResult()):
         header("location: admin.php");
     endif;
 endif;
@@ -32,8 +30,7 @@ if ($dataPost and isset($dataPost['SendSignUp'])):
         $NewUser = new cadUser;
         $dataPost['user_level'] = 10;
         $NewUser->ExeCreate($dataPost);
-        $msgSystem = $NewUser->getError();
-    endif;
+     endif;
 endif;
 ?>
 
@@ -60,69 +57,73 @@ endif;
 
     <body class="login-page">
         <div class="container">
-            <div class="row">
-                <h1>Controle Financeiro Familiar</h1>
-            </div>
+          <div class="row">
+            <h1>Controle Financeiro Familiar</h1>
+          </div>
 
-            <div id="msgSystem" class="">
-                <?= $msgSystem ? $msgSystem : false; ?>
-            </div>
+          <div id="msgSystem" class="">
+              <?php
+              if(!empty($NewUser)):
+                    echo $NewUser->getError();
+              endif;
+              ?>
+          </div>
 
-            <div class="row">
-                <div class="col-md-4 col-md-offset-4">
-                    <div id="loginBox" class="login-box">
-                        <h2>Login</h2>
-                        <form id="" class="" method="POST" action="" enctype="multipart/form-data">
-                            <div id="" class="">
-                                <label for="user_login">Usuário: </label>
-                                <input type="text" class="form-control" name="user_login" id="user_login" placeholder="E-mail/Usuário" required />
-                            </div>
-                            <div id="" class="">
-                                <label for="user_login">Senha: </label>
-                                <input type="password" class="form-control" name="user_password" id="user_password" placeholder="Senha" required />
-                            </div>
-                            <div id="" class="text-center">
-                                <input type="submit" id="" class="btn" name="SendLogin" value="Login">
-                            </div>
-                        </form>
-                        <a id="signupShow" class="">Criar um registro</a>
-                    </div>
-                </div>
+          <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+              <div id="loginBox" class="login-box">
+                  <h2>Login</h2>
+                  <form id="" class="" method="POST" action="" enctype="multipart/form-data">
+                      <div id="" class="">
+                          <label for="user_login">Usuário: </label>
+                          <input type="text" class="form-control" name="user_login" id="user_login" placeholder="E-mail/Usuário" required />
+                      </div>
+                      <div id="" class="">
+                          <label for="user_login">Senha: </label>
+                          <input type="password" class="form-control" name="user_password" id="user_password" placeholder="Senha" required />
+                      </div>
+                      <div id="" class="text-center">
+                          <input type="submit" id="" class="btn" name="SendLogin" value="Login">
+                      </div>
+                  </form>
+                  <a id="signupShow" class="">Criar um registro</a>
+              </div>
             </div>
+          </div>
 
-            <div class="row">
-                <div class="col-md-4 col-md-offset-4">
-                    <div id="signupBox" class="login-box">
-                        <h2>Cadastro</h2>
-                        <form id="" class="" method="POST" action="" enctype="multipart/form-data">
-                            <div id="" class="">
-                                <label for="user_name">Nome: </label>
-                                <input type="text" class="form-control" name="user_name" id="user_name" placeholder="Nome" required />
-                            </div>
-                            <div id="" class="">
-                                <label for="user_name">Login/Usuário: </label>
-                                <input type="text" class="form-control" name="user_login" id="user_login" placeholder="Usuário" required />
-                            </div>
-                            <div id="" class="">
-                                <label for="user_email">E-mail: </label>
-                                <input type="text" class="form-control" name="user_email" id="user_email" placeholder="E-mail" required />
-                            </div>
-                            <div id="" class="">
-                                <label for="user_password">Senha: </label>
-                                <input type="password" class="form-control" name="user_password" id="user_password" placeholder="Senha" required />
-                            </div>
-                            <div id="" class="">
-                                <label for="user_password">Repita a Senha: </label>
-                                <input type="password" class="form-control" name="user_password2" id="user_password" placeholder="Senha" required />
-                            </div>
-                            <div id="" class="text-center">
-                                <input type="submit" id="" class="btn" name="SendSignUp" value="Cadastrar">
-                            </div>
-                        </form>
-                        <a id="loginShow" class="">Fazer Login</a>
-                    </div>
-                </div>
+          <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+              <div id="signupBox" class="login-box">
+                  <h2>Cadastro</h2>
+                  <form id="" class="" method="POST" action="" enctype="multipart/form-data">
+                      <div id="" class="">
+                          <label for="user_name">Nome: </label>
+                          <input type="text" class="form-control" name="user_name" id="user_name" placeholder="Nome" required />
+                      </div>
+                      <div id="" class="">
+                          <label for="user_name">Login/Usuário: </label>
+                          <input type="text" class="form-control" name="user_login" id="user_login" placeholder="Usuário" required />
+                      </div>
+                      <div id="" class="">
+                          <label for="user_email">E-mail: </label>
+                          <input type="text" class="form-control" name="user_email" id="user_email" placeholder="E-mail" required />
+                      </div>
+                      <div id="" class="">
+                          <label for="user_password">Senha: </label>
+                          <input type="password" class="form-control" name="user_password" id="user_password" placeholder="Senha" required />
+                      </div>
+                      <div id="" class="">
+                          <label for="user_password">Repita a Senha: </label>
+                          <input type="password" class="form-control" name="user_password2" id="user_password" placeholder="Senha" required />
+                      </div>
+                      <div id="" class="text-center">
+                          <input type="submit" id="" class="btn" name="SendSignUp" value="Cadastrar">
+                      </div>
+                  </form>
+                  <a id="loginShow" class="">Fazer Login</a>
+              </div>
             </div>
+          </div>
         </div>
     </body>
 </html>
